a11fa4b321831f6681005b8925149d593d71c632
Kartik Sharma <98kartik.sharma@gmail.com>
2019-10-04 20:07:20 +0530

    Delete chal2.py

--- forensics/stego2/chal2.py
+++
@@ -1,9 +0,0 @@
-import binascii
-filename = 'original.png'
-with open(filename, 'rb') as f:
-    content = f.read()
-content=binascii.hexlify(content)
-content=content[::-1]
-print content
-file2 = open(r"new","w+") 
-file2.write(content)
